6231e7d69029fb8bd197caf2214d4bd84a8724ae
ag3112 <ag_3112@outlook.com>
2015-06-27 21:09:32 +0530

    Atm Pin Class

---
+++ core-java/src/main/java/com/practice/examples/AtmPin.java
@@ -0,0 +1,19 @@
+package com.practice.examples;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by Intel on 6/27/2015.
+ */
+public class AtmPin {
+
+    public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("0000.#");
+
+        for (int i = 0; i <= 9999; i++) {
+            System.out.println(df.format(i));
+        }
+
+    }
+}
