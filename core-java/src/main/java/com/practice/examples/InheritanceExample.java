c3125338f763c4db11d5ad60335045998901a08c
ag3112 <ag_3112@outlook.com>
2015-06-28 18:23:23 +0530

    Added Comments

--- core-java/src/main/java/com/practice/examples/InheritanceExample.java
+++ core-java/src/main/java/com/practice/examples/InheritanceExample.java
@@ -10,36 +10,41 @@ public class InheritanceExample {
         A a = new A();
         System.out.println(a.a);
         a.message();
+
+        /* a does not know dummy method so can not access it */
 //        a.dummy();
 
         A aref;
 
         System.out.println("\nNow with reference");
         aref = new B();
+        /*As aref is the reference to base class a and is pointing to sub class. Base class 'A' can not access dummy method*/
 //        aref.dummy();
+
         System.out.println(aref.a);
         aref.message();
 
         System.out.println("\n" +
                 "B object");
+
         B b = new B();
         System.out.println(b.a);
         b.message();
-        b.dummy();
+        b.dummy(); // Perfect, Dummy is inside b so it's right
     }
 }
 
-class A{
+class A {
 
     int a = 10;
 
-    public void message(){
+    public void message() {
         System.out.println("a");
     }
 
 }
 
-class B extends A{
+class B extends A {
 
     int a = 20;
 
@@ -48,7 +53,7 @@ class B extends A{
         System.out.println("b");
     }
 
-    public void dummy(){
+    public void dummy() {
         System.out.println("dummy");
     }
 }
