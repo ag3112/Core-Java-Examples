a61d344559e0c4623ca7f74246a0241a2274a71d
ag3112 <ag_3112@outlook.com>
2015-06-28 14:37:20 +0530

    Added example file

---
+++ core-java/src/main/java/com/practice/examples/CodingExample.java
@@ -0,0 +1,14 @@
+package com.practice.examples;
+
+/**
+ * Created by Intel on 6/28/2015.
+ */
+public class CodingExample {
+    public static void main(String[] args) {
+        int x = 010;
+        short s = 050;
+        s += x;
+        System.out.println(s);
+    }
+}
+
